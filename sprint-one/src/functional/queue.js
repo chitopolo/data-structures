5769a0df33a0a50d3b35a8c52dd91e372aa0403a
chitopolo <renejpolo@me.com>
2014-10-29 15:08:46 -0700

    Finished first functional - data structure

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,31 @@
 var makeQueue = function(){
   var someInstance = {};
-
+  var count = 0;
+  var first = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[count] = value;
+    count++;
+
   };
 
   someInstance.dequeue = function(){
+    var deqVal = storage[first];
+    delete storage[first];
+    first++;
+    return deqVal;
+
   };
 
   someInstance.size = function(){
+    if(first < count){
+      return count - first;
+    }
+      return 0;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,32 @@
 var makeStack = function(){
   var someInstance = {};
-
+  var count = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[count]= value;
+    count++;
   };
 
   someInstance.pop = function(){
+    if (count > 0){
+      count--;
+      var popValue = storage[count];
+      delete storage[count];
+      return popValue;
+    }
+
   };
 
   someInstance.size = function(){
+    if(count === 0){
+      return 0;
+    }else{
+      return count;
+    }
+
   };
 
   return someInstance;
