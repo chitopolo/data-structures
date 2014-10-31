57949750e58f50c9a5c1987703ee7d95ea19138e
chitopolo <renejpolo@me.com>
2014-10-30 19:46:41 -0700

    finished linked-list, set

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,29 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    if(list.head === null){
+      list.head = makeNode(value);
+      list.tail = list.head;
+    }else{
+      list.tail.next = makeNode(value);
+      list.tail = list.tail.next;
+    }
   };
 
   list.removeHead = function(){
+    var curHead = list.head.value;
+    list.head = list.head.next;
+    return curHead;
   };
 
-  list.contains = function(target){
+  list.contains = function(target, startPoint){
+    startPoint = startPoint || list.head;
+    if(startPoint.value === target){
+      return true;
+    }else if(startPoint.next !== null){
+      return list.contains(target, startPoint.next);
+    }
+    return false;
   };
 
   return list;
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,40 @@
 var makeSet = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  //set._storage = undefined;
+  set._storage = {};
+  set._size = 0;
+
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  if(!this.contains(item)){
+      this._storage[this._size] = item;
+      this._size++;
+  }
 };
 
 setPrototype.contains = function(item){
+  for(var k in this._storage){
+    if(this._storage[k] === item){
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item){
+  if(this.contains(item)){
+    for(var k in this._storage){
+      if(this._storage[k] === item){
+        delete this._storage[k];
+      }
+    }
+  }
+
+  this._size--;
 };
 
 /*
