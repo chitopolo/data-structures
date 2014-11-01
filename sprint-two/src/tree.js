9e11e01459dac0e409f1cf41300388393976bb56
Rick Takes <rick@theparkmart.com>
2014-11-01 07:20:39 -0700

    Solved tree, needs cleanup

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -15,8 +15,14 @@ treeMethods.addChild = function(value){
 treeMethods.contains = function(target){
 
   var found = false;
+   var current = this;
 
-  var contains = walkTheDOM(this, target);
+
+
+   found = findChildRecursively(this, target);
+   debugger;
+
+  //var contains = walkTheDOM(this, target);
 
   // var contains = walkTheDOM(this, function(node){
   //   if(node.value === target){
@@ -30,22 +36,21 @@ treeMethods.contains = function(target){
 
 };
 
-  function walkTheDOM(node, target) {
-    ;
-      if(node.value === target){
-        return true;
-      }
-
-      node = node.children[0];
-      var i = 1;
-      while (node) {
-
-          walkTheDOM(node, func);
-          node = node.children[i]; //next sibling
-          i++;
-      }
-      return false;
-  }
+function findChildRecursively(tree, value) { 
+    var cs = tree.children; 
+    for(var i = 0; i < tree.children.length; i++) { 
+        if(cs[i].value === value){ 
+            return true; 
+        } 
+        else { 
+            // Find it in this tree 
+            if(found = findChildRecursively(cs[i], value)) { 
+                return found; 
+            } 
+        } 
+    } 
+    return false; 
+}  
 
 
 
