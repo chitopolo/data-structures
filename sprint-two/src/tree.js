46fd75841e301245bde9f6e87c8864d24a8cd1dc
Rick Takes <rick@theparkmart.com>
2014-10-31 22:03:07 -0700

    Added walk the dom

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -12,21 +12,36 @@ treeMethods.addChild = function(value){
    this.children.push(makeTree(value));
 };
 
-treeMethods.contains = function(target, node){
+treeMethods.contains = function(target){
 
-   node = node || this;
    debugger;
-   if(node.value && node.value === target ) {
-    return true;
-   }
-   if(node.children.length> 0){
-     for(var i = 0; i<node.children.length; i++){
 
-      return node.contains(target, node.children[i] );
-     }
-   }
-   return false;
- };
+  var contains = walkTheDOM(this, function(node){
+      if(node.value === target){
+        return true;
+      }
+      return false;
+   });
+
+  return contains;
+
+  
+  function walkTheDOM(node, func) {
+      var cont = func(node);
+      if(cont === true){
+        return true;
+      }
+
+      node = node.children[0];
+      var i = 1;
+      while (node) {
+          walkTheDOM(node, func);
+          node = node.children[i]; //next sibling
+          i++;
+      }
+      return false;
+  }
+};
 
 
 /*
